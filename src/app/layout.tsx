8f8b493eba7bf8dc6fccb489eed3d3f5eeeda7cc
Victor Azevedo <victorazesc@hotmail.com>
2025-09-28 00:02:37 -0300

    feat: vercel analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-slate-50 text-slate-900 antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
